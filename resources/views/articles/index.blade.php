78be13e18fab729490886dce5ea9cbc5221c9311
deividasja <deividas.jagela@stud.vilniustech.lt>
2023-04-14 13:54:22 +0300

    added a delete session message

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <h2>List of articles</h2>
+    @if(session('status'))
+        <div style="background-color: green; color: lime;">{{ session('status') }}</div>
+    @endif
     <a href="{{ route('articles.create') }}"><button type="button">Create article</button></a>
     @each('articles.partials.list', $articles, 'article')
 @endsection
